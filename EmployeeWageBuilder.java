b64bd9a4e5453549b82e7dff7780cb0dfc79ea39
YashG0728 <107634548+YashG0728@users.noreply.github.com>
2022-07-02 13:51:12 +0530

    Update EmployeeWageBuilder.java

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -2,31 +2,39 @@ package employeewage;
 
 public class EmployeeWageBuilder {
 
+	public static final int FullTime=1;
+	public static final int PartTime=2;
+	public static final int wagePerHour=20;
+	public static final int NumOfWorkingDays=20;
+	
 	public static void main(String[] args) {
 		
-		final int FullTime=1;
-		final int PartTime=2;
-		final int wagePerHour=20;
-		int workingHrs=0;
-		int empWage=0;
-		        
-		int empCheck=(int)Math.floor(Math.random()*10)%3;
-		switch (empCheck)
-		{
-		case FullTime:
-	        System.out.println("Employee is Present Full time");
-		    workingHrs=8;
-		    break;
-		case PartTime:
-		    System.out.println("Employee is Present Part time");
-		    workingHrs=4;
-		    break;
-		default:
-		    System.out.println("Employee is Absent");    
-		}
-		empWage=workingHrs*wagePerHour;
-		    System.out.println("Employee Daily Wage is " +empWage);
-
-		}
-
+	   int workingHrs=0;
+	   int empWage=0;
+	   int totalEmpWage=0;
+	   
+	   for (int day=0; day<NumOfWorkingDays; day++) {
+		   int empCheck=(int)Math.floor(Math.random()*10)%3;
+		   switch (empCheck) {
+		   case FullTime:
+			   workingHrs=8;
+			   break;
+			   
+		   case PartTime:
+			   workingHrs=4;
+			   break;
+			   
+		   default:
+			   workingHrs=0;
+		  }
+          
+		  empWage=workingHrs*wagePerHour;
+		  totalEmpWage +=empWage;
+		  System.out.println("Employee wage="+empWage);
+		  
+	      }
+	   
+          System.out.println("Total employee wage="+totalEmpWage);
 	}
+
+}
