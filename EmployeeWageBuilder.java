c5e75e5542c6db485b98579b9b556a4d8efc36a9
yash <107634548+YashG0728@users.noreply.github.com>
2022-07-07 21:55:16 +0530

    Update EmployeeWageBuilder.java

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,19 +1,35 @@
 package employeewage;
 
 public class EmployeeWageBuilder {
+	
+	public static final int FullTime=1;
+	public static final int PartTime=2;
+	public static final int wagePerHour=20;
+	public static final int NumOfWorkingDays=20;
+	public static final int MaxWorkingHrs=100;
 
 	public static void main(String[] args) {
-			//CONSTANTS
-			int IS_PRESENT=1;
-			double empCheck=Math.floor(Math.random()*10)%2;
-			
-			if(empCheck==IS_PRESENT) {
-				System.out.println("Employee is present");
+		
+		int workingHrs=0, totalWorkingHrs=0, totalWorkingDays=0;
+		while (totalWorkingHrs<=MaxWorkingHrs && totalWorkingDays<=NumOfWorkingDays) {
+			totalWorkingDays++;
+			int empCheck=(int)Math.floor(Math.random()*10)%3;
+			switch (empCheck) {
+			case FullTime:
+				workingHrs=8;
+				break;
+				
+			case PartTime:
+				workingHrs=4;
+				break;
+				
+			default:
+				workingHrs=0;
 			}
-			else  {
-				System.out.println("Employee is absent");
+			totalWorkingHrs +=workingHrs;
+		    System.out.println("Day "+totalWorkingDays+" Employee working hours= "+workingHrs);
 			}
-
+		int totalEmpWage = totalWorkingHrs * wagePerHour;
+		System.out.println("Total Emp Wage: " + totalEmpWage);
 	}
-
 }
