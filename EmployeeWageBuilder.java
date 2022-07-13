0feab35d8a77c3a2f3c47d1480e880273e8f2f01
yash <107634548+YashG0728@users.noreply.github.com>
2022-07-13 20:21:23 +0530

    Update EmployeeWageBuilder.java

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,19 +1,44 @@
-package employeewage;
+package com.bridgelabz;
 
-public class EmployeeWageBuilder {
+public class EmployeeWageBuilderUC7 {
+    public static final int IS_FULL_TIME = 2;
+    public static final int IS_PART_TIME = 1;
+    public static final int EMP_RATE_PER_HOUR = 20;
+    public static final int NUM_OF_WORKING_DAYS = 20;
+    public static final int MAX_HOURS_IN_MONTH = 100;
 
-	public static void main(String[] args) {
-			//CONSTANTS
-			int IS_PRESENT=1;
-			double empCheck=Math.floor(Math.random()*10)%2;
-			
-			if(empCheck==IS_PRESENT) {
-				System.out.println("Employee is present");
-			}
-			else  {
-				System.out.println("Employee is absent");
-			}
+    public static int employeeWageTillCondition(){
+        //Variables
+        int empHours = 0;
+        int totalEmpHours= 0;
+        int totalWorkingDays= 0;
+        //Computation
+        while ( totalEmpHours < MAX_HOURS_IN_MONTH &&
+                totalWorkingDays < NUM_OF_WORKING_DAYS)
+        {
+            totalWorkingDays++;
+            int empCheck = (int) Math.floor(Math.random () * 10)%3;
 
-	}
+            switch (empCheck){
+                case IS_FULL_TIME:
+                    empHours = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHours = 4;
+                    break;
+                default:
+                    empHours = 0;
+            }
+            totalEmpHours += empHours;
+            System.out.format("employee hours in day %d is %d \n ",totalWorkingDays,empHours );
+        }
+        System.out.format("totalEmpHours in %d days is %d \n ",totalWorkingDays,totalEmpHours );
+        int totalEmpWage = totalEmpHours * EMP_RATE_PER_HOUR;
+        System.out.println("Total Emp Wage is " + totalEmpWage );
+        return totalEmpWage;
+    }
 
+    public static void main(String[] args) {
+        employeeWageTillCondition();
+    }
 }
