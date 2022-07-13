55b24fdf8f5ee32d8accb86dfebd7a8e5d9d5279
yash <107634548+YashG0728@users.noreply.github.com>
2022-07-13 20:27:18 +0530

    Update EmployeeWageBuilder.java

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,20 +1,33 @@
+
 package employeewage;
+
 import java.util.Random;
-public class EmployeeWageBuilder {
+
+public class EmployeeWageComputation {
 
     public static final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
-//    public static final int EMP_RATE_PER_HOUR = 20;
-//    private static final int NUM_WORKING_DAYS = 20;
-//    private static final int MAX_WORKING_HRS_IN_MONTH = 100;
 
-    public static int computeEmpWage(String companyName, int EMP_RATE_PER_HOUR, int NUM_WORKING_DAYS, int MAX_WORKING_HRS_IN_MONTH) {
+    private final String companyName;
+    private final int EMP_RATE_PER_HOUR;
+    private final int NUM_WORKING_DAYS;
+    private final int MAX_WORKING_HRS_IN_MONTH;
+    private int totalEmpWage;
+
+    public EmployeeWageComputation(String companyName, int EMP_RATE_PER_HOUR, int NUM_WORKING_DAYS, int MAX_WORKING_HRS_IN_MONTH) {
+        this.companyName = companyName;
+        this.EMP_RATE_PER_HOUR = EMP_RATE_PER_HOUR;
+        this.NUM_WORKING_DAYS = NUM_WORKING_DAYS;
+        this.MAX_WORKING_HRS_IN_MONTH = MAX_WORKING_HRS_IN_MONTH;
+    }
+    public void computeEmpWage() {
         Random random = new Random();
-        int empHrs;
+        int empHrs = 0;
         int totalEmpHrs = 0;
         int totalWorkingDays = 0;
-        System.out.println(" --------------------------------");
-        System.out.println("Name of the company :- "+companyName);
+        System.out.println("----------------------------------");
+        System.out.println("Name of the company :- " + companyName);
+        System.out.println("Employee rate per hour "+EMP_RATE_PER_HOUR);
 
         while (totalEmpHrs <= MAX_WORKING_HRS_IN_MONTH && totalWorkingDays < NUM_WORKING_DAYS) {
             totalWorkingDays++;
@@ -32,18 +45,28 @@ public class EmployeeWageBuilder {
             totalEmpHrs += empHrs;
             System.out.println("Day " + totalWorkingDays + " Employee working hours : " + empHrs);
         }
-        int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-        System.out.println("Total employee wage : " + totalEmpWage);
-        return totalEmpWage;
+
+         totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
     }
 
+    @Override
+    public String toString() {
+        return "Total employee wage :-- " + totalEmpWage;
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to the employee wage computation program");
-        computeEmpWage("Amazon",100, 20, 100);
-        computeEmpWage("TCS",80, 15,100 );
-        computeEmpWage("BridgeLabz", 50, 20, 80);
-        computeEmpWage("ITC", 40, 10, 60);
-
+        EmployeeWageComputation amazon = new EmployeeWageComputation("Amazon", 20,
+                4,50);
+        EmployeeWageComputation tcs = new EmployeeWageComputation("TCS", 10,
+                4, 40);
+        EmployeeWageComputation tesla = new EmployeeWageComputation("TESLA", 40,
+                 5, 25);
+        amazon.computeEmpWage();
+        System.out.println(amazon);
+        tcs.computeEmpWage();
+        System.out.println(tcs);
+        tesla.computeEmpWage();
+        System.out.println(tesla);
     }
 
 }
