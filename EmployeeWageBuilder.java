ce34b4391ca661f36179327ba6f600a2d559c25e
yash <107634548+YashG0728@users.noreply.github.com>
2022-07-13 20:41:02 +0530

    Update EmployeeWageBuilder.java

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,88 +1,100 @@
 package employeewage;
 
-    public class EmployeeWageBuilder {
-        final int is_Part_Time = 1;
-        final int is_Full_Time = 2;
+import java.util.ArrayList;
 
-        public void wageComputation(Companyempwage companyEmployee){
+interface EmployeeWageBuilder {
 
-            int empHrs = 0;
-            int totalEmpHrs = 0;
-            int totalWorkDay = 0;
-            int totalEmpWage = 0;
+    public void wageComputation( Companyempwage obj );
+}
 
-            while (totalWorkDay < companyEmployee.getNumOfWorkingDays() && totalEmpHrs <= companyEmployee.getMaxHrsInMonth()) {
 
-                int employee = (int) ((Math.random() * 10) % 3);
+public class EmployeeWage implements EmpWageBuilder{
 
-                switch (employee) {
-                    case is_Part_Time:
-                        empHrs = 4;
-                        break;
-                    case is_Full_Time:
-                        empHrs = 8;
-                        break;
-                    default:
-                        empHrs = 0;
-                }
-                totalWorkDay++;
-                totalEmpHrs = totalEmpHrs + empHrs;
+    final int is_Part_Time = 1;
+    final int is_Full_Time = 2;
+
+    public void wageComputation(Companyempwage companyEmployee){
+
+        int empHrs = 0;
+        int totalEmpHrs = 0;
+        int totalWorkDay = 0;
+        int totalEmpWage = 0;
+
+        while (totalWorkDay < companyEmployee.getNumOfWorkingDays() && totalEmpHrs <= companyEmployee.getMaxHrsInMonth()) {
+
+            int employee = (int) ((Math.random() * 10) % 3);
+
+            switch (employee) {
+                case is_Part_Time:
+                    empHrs = 4;
+                    break;
+                case is_Full_Time:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
             }
-            companyEmployee.setTotalEmpWage( totalEmpHrs * companyEmployee.empRatePerHr );
-            System.out.println("Employee Monthly Wage of " + companyEmployee.getCompanyName() + " is " + companyEmployee.getTotalEmpWage());
+            totalWorkDay++;
+            totalEmpHrs = totalEmpHrs + empHrs;
         }
-        public static void main(String arg[]) {
-
-            EmployeeWage emp = new EmployeeWage();
-
-            Companyempwage[] company = new Companyempwage[10];
-
-            company[0] = new Companyempwage("Asus", 20, 20, 100);
-            emp.wageComputation(company[0]);
-            company[1] = new Companyempwage("Dell", 25, 22, 120);
-            emp.wageComputation(company[1]);
-            company[2] = new Companyempwage("HP", 30, 18, 90);
-            emp.wageComputation(company[2]);
-        }
+        companyEmployee.setTotalEmpWage( totalEmpHrs * companyEmployee.empRatePerHr );
+        System.out.println("Employee Monthly Wage of " + companyEmployee.getCompanyName() + " is " + companyEmployee.getTotalEmpWage());
     }
 
-    class Companyempwage{
+    public static void main(String arg[]) {
 
-        public String companyName=" ";
-        public int empRatePerHr=0;
-        public int noOfWorkDay=0;
-        public int maxHrsInMonth=0;
+        EmployeeWage emp = new EmployeeWage();
 
-        public int totalEmpWage=0;
+        ArrayList<Companyempwage> company = new ArrayList<Companyempwage>();
 
-        public Companyempwage(String companyName, int empRatePerHr, int noOfWorkDay, int maxHrsInMonth){
-            this.companyName=companyName;
-            this.empRatePerHr=empRatePerHr;
-            this.noOfWorkDay=noOfWorkDay;
-            this.maxHrsInMonth=maxHrsInMonth;
-        }
+        company.add( new Companyempwage( "Asus", 40, 20, 100 ));
+        emp.wageComputation(company.get(0));
 
-        public String getCompanyName(){
-            return companyName;
-        }
-        public int getEmpRatePerHour(){
-            return empRatePerHr;
-        }
+        company.add( new Companyempwage( "Dell", 30, 22, 120 ));
+        emp.wageComputation(company.get(1));
 
-        public int getNumOfWorkingDays(){
-            return noOfWorkDay;
-        }
-
-        public int getMaxHrsInMonth(){
-            return maxHrsInMonth;
-        }
-
-        public void setTotalEmpWage( int totalEmpWage ){
-            this.totalEmpWage = totalEmpWage;
-        }
-
-        public int getTotalEmpWage(){
-            return totalEmpWage;
-        }
-
+        company.add( new Companyempwage( "HP", 45, 18, 90 ));
+        emp.wageComputation(company.get(2));
     }
+}
+
+class Companyempwage{
+
+    public String companyName=" ";
+    public int empRatePerHr=0;
+    public int noOfWorkDay=0;
+    public int maxHrsInMonth=0;
+
+    public int totalEmpWage=0;
+
+    public Companyempwage(String companyName, int empRatePerHr, int noOfWorkDay, int maxHrsInMonth){
+        this.companyName=companyName;
+        this.empRatePerHr=empRatePerHr;
+        this.noOfWorkDay=noOfWorkDay;
+        this.maxHrsInMonth=maxHrsInMonth;
+    }
+
+    public String getCompanyName(){
+        return companyName;
+    }
+    public int getEmpRatePerHour(){
+        return empRatePerHr;
+    }
+
+    public int getNumOfWorkingDays(){
+        return noOfWorkDay;
+    }
+
+    public int getMaxHrsInMonth(){
+        return maxHrsInMonth;
+    }
+
+    public void setTotalEmpWage( int totalEmpWage ){
+        this.totalEmpWage = totalEmpWage;
+    }
+
+    public int getTotalEmpWage(){
+        return totalEmpWage;
+    }
+
+}
