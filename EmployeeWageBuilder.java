593b4b1d56986ceb326b9cc18f6acdf365d7e584
yash <107634548+YashG0728@users.noreply.github.com>
2022-07-13 20:25:27 +0530

    Update EmployeeWageBuilder.java

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,44 +1,49 @@
 package employeewage;
-
+import java.util.Random;
 public class EmployeeWageBuilder {
-    public static final int IS_FULL_TIME = 2;
-    public static final int IS_PART_TIME = 1;
-    public static final int EMP_RATE_PER_HOUR = 20;
-    public static final int NUM_OF_WORKING_DAYS = 20;
-    public static final int MAX_HOURS_IN_MONTH = 100;
 
-    public static int employeeWageTillCondition(){
-        //Variables
-        int empHours = 0;
-        int totalEmpHours= 0;
-        int totalWorkingDays= 0;
-        //Computation
-        while ( totalEmpHours < MAX_HOURS_IN_MONTH &&
-                totalWorkingDays < NUM_OF_WORKING_DAYS)
-        {
+    public static final int IS_FULL_TIME = 1;
+    public static final int IS_PART_TIME = 2;
+//    public static final int EMP_RATE_PER_HOUR = 20;
+//    private static final int NUM_WORKING_DAYS = 20;
+//    private static final int MAX_WORKING_HRS_IN_MONTH = 100;
+
+    public static int computeEmpWage(String companyName, int EMP_RATE_PER_HOUR, int NUM_WORKING_DAYS, int MAX_WORKING_HRS_IN_MONTH) {
+        Random random = new Random();
+        int empHrs;
+        int totalEmpHrs = 0;
+        int totalWorkingDays = 0;
+        System.out.println(" --------------------------------");
+        System.out.println("Name of the company :- "+companyName);
+
+        while (totalEmpHrs <= MAX_WORKING_HRS_IN_MONTH && totalWorkingDays < NUM_WORKING_DAYS) {
             totalWorkingDays++;
-            int empCheck = (int) Math.floor(Math.random () * 10)%3;
-
-            switch (empCheck){
+            int employeeCheck = random.nextInt(3);
+            switch (employeeCheck) {
                 case IS_FULL_TIME:
-                    empHours = 8;
+                    empHrs = 8;
                     break;
                 case IS_PART_TIME:
-                    empHours = 4;
+                    empHrs = 4;
                     break;
                 default:
-                    empHours = 0;
+                    empHrs = 0;
             }
-            totalEmpHours += empHours;
-            System.out.format("employee hours in day %d is %d \n ",totalWorkingDays,empHours );
+            totalEmpHrs += empHrs;
+            System.out.println("Day " + totalWorkingDays + " Employee working hours : " + empHrs);
         }
-        System.out.format("totalEmpHours in %d days is %d \n ",totalWorkingDays,totalEmpHours );
-        int totalEmpWage = totalEmpHours * EMP_RATE_PER_HOUR;
-        System.out.println("Total Emp Wage is " + totalEmpWage );
+        int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Total employee wage : " + totalEmpWage);
         return totalEmpWage;
     }
 
     public static void main(String[] args) {
-        employeeWageTillCondition();
+        System.out.println("Welcome to the employee wage computation program");
+        computeEmpWage("Amazon",100, 20, 100);
+        computeEmpWage("TCS",80, 15,100 );
+        computeEmpWage("BridgeLabz", 50, 20, 80);
+        computeEmpWage("ITC", 40, 10, 60);
+
     }
+
 }
