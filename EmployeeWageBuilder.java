1d01f2bf305fa7f9a04f73ddc0907a223a34ab46
yash <107634548+YashG0728@users.noreply.github.com>
2022-07-13 20:43:44 +0530

    Update EmployeeWageBuilder.java

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -13,13 +13,16 @@ public class EmployeeWage implements EmpWageBuilder{
     final int is_Part_Time = 1;
     final int is_Full_Time = 2;
 
+    ArrayList<Integer> empDailyMonthlyTotalWage = new ArrayList<Integer>();
+
     public void wageComputation(Companyempwage companyEmployee){
+        System.out.println(companyEmployee.getCompanyName()+" Employee Wage Computation");
 
         int empHrs = 0;
         int totalEmpHrs = 0;
         int totalWorkDay = 0;
         int totalEmpWage = 0;
-
+        int empDailyWage = 0;
         while (totalWorkDay < companyEmployee.getNumOfWorkingDays() && totalEmpHrs <= companyEmployee.getMaxHrsInMonth()) {
 
             int employee = (int) ((Math.random() * 10) % 3);
@@ -36,23 +39,29 @@ public class EmployeeWage implements EmpWageBuilder{
             }
             totalWorkDay++;
             totalEmpHrs = totalEmpHrs + empHrs;
+            empDailyWage = empHrs * companyEmployee.getEmpRatePerHour();
+            empDailyMonthlyTotalWage.add( empDailyWage );
+            System.out.println("Day:"+totalWorkDay +" Wage is : "+empDailyWage );
         }
-        companyEmployee.setTotalEmpWage( totalEmpHrs * companyEmployee.empRatePerHr );
-        System.out.println("Employee Monthly Wage of " + companyEmployee.getCompanyName() + " is " + companyEmployee.getTotalEmpWage());
+        totalEmpWage=( totalEmpHrs * companyEmployee.getEmpRatePerHour());
+        empDailyMonthlyTotalWage.add( totalEmpWage );
+        companyEmployee.setTotalEmpWage( totalEmpWage );
+        System.out.println("Employee Monthly Total  Wage of " + companyEmployee.getCompanyName() + " is " + companyEmployee.getTotalEmpWage());
     }
 
     public static void main(String arg[]) {
 
+        System.out.println();
         EmployeeWage emp = new EmployeeWage();
 
         ArrayList<Companyempwage> company = new ArrayList<Companyempwage>();
 
         company.add( new Companyempwage( "Asus", 40, 20, 100 ));
         emp.wageComputation(company.get(0));
-
+        System.out.println();
         company.add( new Companyempwage( "Dell", 30, 22, 120 ));
         emp.wageComputation(company.get(1));
-
+        System.out.println();
         company.add( new Companyempwage( "HP", 45, 18, 90 ));
         emp.wageComputation(company.get(2));
     }
